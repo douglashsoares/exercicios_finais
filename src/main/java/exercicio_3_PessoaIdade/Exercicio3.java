710542ae9210b0ac1cf1ffc3470a8cd8762ec552
Antonio Monteiro <antoniohbmonteiro@gmail.com>
2019-09-02 20:24:41 -0300

    Consertado

--- src/main/java/exercicio_3_PessoaIdade/Exercicio3.java
+++ src/main/java/exercicio_3_PessoaIdade/Exercicio3.java
@@ -6,12 +6,11 @@ import classes.Pessoa;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Exercicio3 extends ExercicioBase{
+public class Exercicio3 extends ExercicioBase {
 
-    private Random random = new Random();
-    private Scanner teclado = new Scanner(System.in);
 
-    public Pessoa criarPessoaAleatoria( int p){
+    public Pessoa criarPessoaAleatoria(int p) {
+        Random random = new Random();
         Pessoa pessoa = new Pessoa();
         pessoa.setNome("Pessoa" + p);
         pessoa.setIdade(random.nextInt(40));
@@ -20,15 +19,16 @@ public class Exercicio3 extends ExercicioBase{
 
     @Override
     public void iniciarExercicio() {
+        Scanner teclado = new Scanner(System.in);
         System.out.println("Digite a quantidade de pessoas: ");
         int quantP = teclado.nextInt();
 
         Pessoa[] pessoaAle = new Pessoa[quantP];
 
-        for (int i = 0; i < quantP; i++){
+        for (int i = 0; i < quantP; i++) {
             pessoaAle[i] = criarPessoaAleatoria(i);
         }
-        for (int i = 0;i < quantP; i++){
+        for (int i = 0; i < quantP; i++) {
             pessoaAle[i].status();
         }
 
